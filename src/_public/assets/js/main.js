15fb0bd2389ac19d15824f0297f759bacf990e15
vanbinhagl <vanbinh.agl@gmail.com>
2023-02-23 14:06:38 +0700

    q&a

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("main.js");
+$(".c-usage__viewmore").click(function () {
+  $(this)
+    .parent()
+    .parent()
+    .find(".c-usage__imageconent")
+    .removeClass("is-showContent");
+  $(this).parent().find(".c-usage__imageconent").addClass("is-showContent");
+});
